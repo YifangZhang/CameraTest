9df9146dc81e64769f5cc155b694050b440a93b0
YifangZhang <yifangzhang2009@gmail.com>
2016-08-28 15:13:16 -0500

    Initial Commit

---
+++ CameraTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraTest
+//
+//  Created by Yifang Zhang on 8/28/16.
+//  Copyright © 2016 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraTest
+//
+//  Created by Yifang Zhang on 8/28/16.
+//  Copyright © 2016 Yifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
